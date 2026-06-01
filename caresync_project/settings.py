12ca98c3ad4c78bf61bb98182888cb7f4491f3a9
Your Name <you@example.com>
2026-06-01 17:17:37 +0530

    Update Gmail App Password and revert demo OTP hints for clean production launch

--- caresync_project/settings.py
+++ caresync_project/settings.py
@@ -139,7 +139,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'caresync.support@gmail.com'
-EMAIL_HOST_PASSWORD = 'lcda pbcw easi gjon'
+EMAIL_HOST_PASSWORD = 'ybeh pjdo ijbg eoxe'
 
 if EMAIL_HOST_PASSWORD:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
